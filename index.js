a8d9978261dc54e5e905c40e37842aae4d6fb565
Sathvik <101496703+sathvikcodes@users.noreply.github.com>
2023-02-28 21:23:08 +0530

    Add files via upload
    mobile scaling

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// Navbar for mobiles
+const navbar = document.querySelector('.navbar');
+const menuBtn = document.querySelector('.menu-btn');
+menuBtn.addEventListener('click', () => {
+  navbar.classList.toggle('active');
+});
+menuBtn.addEventListener('click', () => {
+    const miniNav = document.getElementById('mini-nav');
+    if (miniNav.style.display === 'flex') {
+      document.getElementById("mini-nav").style.transform = "translateY(-100%)";
+      document.getElementById("mini-nav").style.transition = "ease 0.8s";
+      setTimeout(function ()
+      {
+        miniNav.style.display = 'none';
+        document.getElementById("mini-nav").style.transform = "translateY(0)";//reset transform property else error occurs
+      },800);
+    } else {
+      miniNav.style.display = 'flex';
+    }
+});  
+
+
+// Aside button
 function openpg()
 {
     document.getElementById("circle").style.display = "inline-block";
@@ -8,6 +31,7 @@ function closepg() {
     document.getElementById("info").style.display = "inline";
 }
 
+// Feedback function
 document.getElementById("feedback-click").addEventListener("click", function()
 {
     document.getElementById("feedback-click").style.display = "none";
@@ -17,7 +41,7 @@ document.getElementById("feedback-click").addEventListener("click", function()
     {
         console.log("inside");
         document.getElementById("feedback-click").style.display = "inline";
-        document.getElementById("inp-form").style.transform = "translateX(-400px)";
+        document.getElementById("inp-form").style.transform = "translateX(-500px)";
         document.getElementById("inp-form").style.transition = "ease 1s";
         setTimeout(function ()
         {
