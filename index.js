caab2c973ee89bd1f67587e57f30166db700767f
Sathvik <101496703+sathvikcodes@users.noreply.github.com>
2023-02-28 21:48:29 +0530

    Add files via upload
    mobile orientation

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 // Navbar for mobiles
 const navbar = document.querySelector('.navbar');
 const menuBtn = document.querySelector('.menu-btn');
+const miniNav = document.getElementById('mini-nav');
 menuBtn.addEventListener('click', () => {
   navbar.classList.toggle('active');
 });
-menuBtn.addEventListener('click', () => {
-    const miniNav = document.getElementById('mini-nav');
+function mini_nav_closer()
+{
+    miniNav.style.display = 'none';
+    document.getElementById("mini-nav").style.transform = "translateY(0)";
+}
+function mini_nav_opener()
+{
     if (miniNav.style.display === 'flex') {
       document.getElementById("mini-nav").style.transform = "translateY(-100%)";
       document.getElementById("mini-nav").style.transition = "ease 0.8s";
       setTimeout(function ()
       {
-        miniNav.style.display = 'none';
-        document.getElementById("mini-nav").style.transform = "translateY(0)";//reset transform property else error occurs
+        mini_nav_closer();//reset transform property else error occurs
       },800);
     } else {
       miniNav.style.display = 'flex';
     }
-});  
+}
+menuBtn.addEventListener('click', mini_nav_opener);  
 
 
 // Aside button
@@ -31,7 +37,7 @@ function closepg() {
     document.getElementById("info").style.display = "inline";
 }
 
-// Feedback function
+// Feedback function computer
 document.getElementById("feedback-click").addEventListener("click", function()
 {
     document.getElementById("feedback-click").style.display = "none";
@@ -41,6 +47,28 @@ document.getElementById("feedback-click").addEventListener("click", function()
     {
         console.log("inside");
         document.getElementById("feedback-click").style.display = "inline";
+        document.getElementById("inp-form").style.transform = "translateX(-500px)";
+        document.getElementById("inp-form").style.transition = "ease 1s";
+        setTimeout(function ()
+        {
+            document.getElementById("inp-form").style.display = "none";
+            document.getElementById("inp-form").style.transform = "translateX(0)"; // reset transform property
+        },500);
+        document.getElementById("close_btn_feedback").style.display = "none";
+    });
+});
+// Feedback function mobile
+document.getElementById("feedback-click1").addEventListener("click", function()
+{
+    document.getElementById("feedback-click1").style.display = "none";
+    navbar.classList.toggle('active');
+    mini_nav_closer();
+    document.getElementById("inp-form").style.display = "inline";
+    document.getElementById("close_btn_feedback").style.display = "block";
+    document.getElementById("close_btn_feedback").addEventListener("click",function()
+    {
+        console.log("inside");
+        document.getElementById("feedback-click1").style.display = "inline";
         document.getElementById("inp-form").style.transform = "translateX(-500px)";
         document.getElementById("inp-form").style.transition = "ease 1s";
         setTimeout(function ()
